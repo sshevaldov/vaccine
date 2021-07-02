ba3d4033b1c83bbb2a000b2b43ba5115c9a16ffb
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-02 23:05:03 +0400

    Update sel.php

--- sel.php
+++ sel.php
@@ -30,13 +30,19 @@ mysqli_set_charset($link, "utf8");
 while($row = mysqli_fetch_array($result))
 {
   echo "<p>Дата рождения: <ins> {$row['birthdate']}";
+  $_SESSION['birthdate']=$row['birthdate'];
   echo "<p>ОМС: <ins>{$row['oms']}";
+  $_SESSION['oms']=$row['oms'];
   echo "<p>Серия/номер паспорта: <ins>{$row['passport']} <p>Код подразделения: <ins>{$row['district_code']}";
+  $_SESSION['district_code']=$row['district_code'];
   echo "<p>Телефон: <ins>{$row['phone']}";
+  $_SESSION['phone']=$row['phone'];
 }
 echo "<h2 style=\"font-weight:bold\">Записан на вакцинацию:</h2>";
 echo "<p>Адрес вакцинации: г. {$_SESSION['city']}, {$_SESSION['place']}";
 echo "<p>Дата и время вакцинации: {$_SESSION['date']} {$_SESSION['time']}";
+$_SESSION['adress']="г. {$_SESSION['city']}, {$_SESSION['place']}";
+$_SESSION['datetime']="{$_SESSION['date']} {$_SESSION['time']}";
 ?>
   
   <p><a href="pdff.php"><button id="button" class="btn_submit disabled">Скачать</button></a>
