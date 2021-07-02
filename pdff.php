63442a6c5b5b3d7a070a6f1de21b6985ede71879
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-02 23:14:17 +0400

    Update pdff.php

--- pdff.php
+++ pdff.php
@@ -8,17 +8,20 @@
       $pdf->SetFont('Arial');       
       $pdf->SetFontSize(25);   
       $pdf->MultiCell(0,0,iconv('utf-8', 'windows-1251',"Табель вакцинации"),0,'C');
+      $pdf->SetFontSize(15); 
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"Пациент"));
       $pdf->SetFontSize(10); 
-      $pdf->MultiCell(0,10,iconv('utf-8', 'windows-1251',"Пациент"));
-      $pdf->MultiCell(0,10,iconv('utf-8', 'windows-1251',"{$_SESSION['surname']} {$_SESSION['name']} {$_SESSION['secondname']}"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Дата рождения: {$_SESSION['birthdate']}"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"ОМС: {$_SESSION['oms']}"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Серия/номер паспорта: {$_SESSION['passport']}"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Код подразделения: {$_SESSION['district_code']}"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Телефон :{$_SESSION['phone']}"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Записан на вакцинацию"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Адрес вакцинации: {$_SESSION['adress']}"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Дата и время: {$_SESSION['datetime']}"));
-      $pdf->Output('iskspb.ru.pdf','I');    
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"{$_SESSION['surname']} {$_SESSION['name']} {$_SESSION['secondname']}"));
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"Дата рождения: {$_SESSION['birthdate']}"));
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"ОМС: {$_SESSION['oms']}"));
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"Серия/номер паспорта: {$_SESSION['passport']}"));
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"Код подразделения: {$_SESSION['district_code']}"));
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"Телефон :{$_SESSION['phone']}"));
+      $pdf->SetFontSize(15); 
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"Записан на вакцинацию"));
+      $pdf->SetFontSize(10); 
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"Адрес вакцинации: {$_SESSION['adress']}"));
+      $pdf->MultiCell(0,8,iconv('utf-8', 'windows-1251',"Дата и время: {$_SESSION['datetime']}"));
+      $pdf->Output('label.pdf','I');   
     
 ?>
