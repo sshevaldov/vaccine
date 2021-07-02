0fe01f23ec5de86baab90f26372a6dbeb97513c7
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-02 23:05:00 +0400

    Update pdff.php

--- pdff.php
+++ pdff.php
@@ -10,15 +10,15 @@
       $pdf->MultiCell(0,0,iconv('utf-8', 'windows-1251',"Табель вакцинации"),0,'C');
       $pdf->SetFontSize(10); 
       $pdf->MultiCell(0,10,iconv('utf-8', 'windows-1251',"Пациент"));
-      $pdf->MultiCell(0,10,iconv('utf-8', 'windows-1251',"Шевалдов Станислав Сергеевич"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Дата рождения: 19.10.2000"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"ОМС :5464 5645 6546 5466"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Серия/номер паспорта: 7320 365666"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Код подразделения: 645-64"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Телефон :+7 (111) 111-11-11"));
+      $pdf->MultiCell(0,10,iconv('utf-8', 'windows-1251',"{$_SESSION['surname']} {$_SESSION['name']} {$_SESSION['secondname']}"));
+      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Дата рождения: {$_SESSION['birthdate']}"));
+      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"ОМС: {$_SESSION['oms']}"));
+      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Серия/номер паспорта: {$_SESSION['passport']}"));
+      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Код подразделения: {$_SESSION['district_code']}"));
+      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Телефон :{$_SESSION['phone']}"));
       $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Записан на вакцинацию"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Адрес вакцинации: г. Ульяновск, ЦГКБ, ул. Оренбургская, 27"));
-      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Дата и время: 07.07.2021 14:50"));
+      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Адрес вакцинации: {$_SESSION['adress']}"));
+      $pdf->MultiCell(0,15,iconv('utf-8', 'windows-1251',"Дата и время: {$_SESSION['datetime']}"));
       $pdf->Output('iskspb.ru.pdf','I');    
     
 ?>
