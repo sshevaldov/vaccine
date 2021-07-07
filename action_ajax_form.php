061215c474586f028b6363280df611c150b91d5b
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-07 19:15:00 +0400

    Update action_ajax_form.php

--- action_ajax_form.php
+++ action_ajax_form.php
@@ -1,27 +1,33 @@
 <?php
 
-if (isset($_POST["passport"]) фтв isset($_POST["password"])) { 
+if (isset($_POST["passport"]) && isset($_POST["password"])) { 
     
+  
+
     $servername = "localhost";
     $uname = "root";
     $pword = "";
     $dbname = "vaccine";    
-$rows1=0;
+    $psw=0;
     $link=mysqli_connect($servername, $uname, $pword, $dbname);  			
     $sql = "SELECT * FROM `accounts` where `passport`='{$_POST["passport"]}'";
     $result=mysqli_query($link,$sql);    
-    $rows = mysqli_num_rows($result);
-    if($rows!=0){//если строки с логином(паспортом) существуют, то...
-    $sql = "SELECT * FROM `accounts` where `passport`='{$_POST["passport"]}' and `password`='{$_POST["password"]}'";
-    $result1=mysqli_query($link,$sql);    
-    $rows1 = mysqli_num_rows($result);//кол-во строк, где логин и пароль сопали
-}
-
-
+    $rows = mysqli_num_rows($result);   
+    if($rows!=0) //если есть строки с подходящим логином
+    {
+       while($row = mysqli_fetch_array($result))
+        {
+            if((password_verify($_POST["password"],$row['password'])))
+            {              
+                $psw=1;               
+            }                      
+        } 
+    }
 	// Формируем массив для JSON ответа
     $result = array(
-    	'name' => $rows, 	
-        'vallog' =>$rows1
+    	'name' => $rows,
+        'psw' => $psw,
+        'h' =>password_hash("{$_POST["password"]}",PASSWORD_DEFAULT)
     ); 
 
     // Переводим массив в JSON
