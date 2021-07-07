1c28557d25406fca28bc9c32d956439bbf79c567
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-07 19:15:06 +0400

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,13 +1,9 @@
 /* Article FructCode.com */
 $( document ).ready(function() {
     $("#button").click(
-		function(){
-			log=document.getElementById('passport').value;
-			
-			if(log !=""){
+		function(){		
 			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
-			return false; }
-			
+			return false;						
 		}
 	);
 });
@@ -20,9 +16,19 @@ function sendAjaxForm(result_form, ajax_form, url) {
         data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
         success: function(response) { //Данные отправлены успешно
         	result = $.parseJSON(response);
-			if(result.name==0)
-			{$("#excp").show();  }
-			else {$("#excp").hide(); }   
+			log=document.getElementById('passport').value;
+			pass=document.getElementById('password').value;	
+			$('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.psw+'<br>Телефон: '+result.h);	
+			
+			if(log !='' && pass !='')
+			if(result.name==0 )
+			{	$("#excp").show();  }
+			else if(result.psw==0) 
+			{
+				$("#excp").hide(); 
+				$("#excp1").show();	
+			} else {$("#excp1").hide(); 
+			$("#excp2").show();	}
 			  
     	},
     	error: function(response) { // Данные не отправлены
