6e12babb8fea14809d88ca9d9c464a262b4fd56f
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-08 22:30:01 +0400

    Update ajax2.js

--- ajax2.js
+++ ajax2.js
@@ -15,31 +15,31 @@ function sendAjaxForm(result_form, ajax_form, url) {
 		data: $("#" + ajax_form).serialize(),  // Сеарилизуем объект
 		success: function (response) { //Данные отправлены успешно
 			result = $.parseJSON(response);
-			$('#result_form').html('Имя: |' + result.length + '|');
+			$('#result_form').html('Имя: |' + result[0] + '|');
 
-		
+
 			//const select = document.createElement('select');
 			$(".pgh").remove();
 			const select = document.getElementById('hh');
-		
+
 			document.querySelector('.weather').insertAdjacentElement('afterBegin', select);
-			
+
 			select.classList.add('weather__list');
 			//select.id = 'city';
-			
-			
-			
+
+
+
 			for (let key in result) {
-				
+
 				const city = result[key];
 				const option = document.createElement('option');
 				option.classList.add('pgh');
 				option.textContent = city;
-				option.value = key;
+				option.value = city;
 				select.appendChild(option);
 
 			}
-
+			document.getElementById("hh").disabled = false;
 
 
 		},
@@ -48,3 +48,57 @@ function sendAjaxForm(result_form, ajax_form, url) {
 		}
 	});
 }
+
+//--------------------------
+/* Article FructCode.com */
+$(document).ready(function () {
+	$("#hh").change(
+		function () {
+			sendAjaxForm1('result_form', 'ajax_form2', 'action_ajax_form3.php');
+		}
+	);
+});
+
+function sendAjaxForm1(result_form, ajax_form, url) {
+	$.ajax({
+		url: url, //url страницы (action_ajax_form1.php)
+		type: "POST", //метод отправки
+		dataType: "html", //формат данных
+		data: $("#" + ajax_form).serialize(),  // Сеарилизуем объект
+		success: function (response) { //Данные отправлены успешно
+			result = $.parseJSON(response);
+			$('#result_form').html('Имя: |' + result + '|');
+
+
+			//const select = document.createElement('select');
+			$(".pgp").remove();
+			const select = document.getElementById('time');
+
+			document.querySelector('.temp').insertAdjacentElement('afterBegin', select);
+
+			select.classList.add('temp__list1');
+			//select.id = 'city';
+
+
+
+			for (let key in result) {
+
+				const city = result[key];
+				const option = document.createElement('option');
+				option.classList.add('pgp');
+				option.textContent = city;
+				option.value = city;
+				
+				select.appendChild(option);
+
+			}
+
+			document.getElementById("time").disabled = false;
+
+		},
+		error: function (response) { // Данные не отправлены
+			$('#excp').html('Ошибка. Данные не отправлены.');
+		}
+	});
+}
+
