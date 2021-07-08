d68ae0ce017c2069b8298a715d96ab3547ce8a2f
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-09 00:14:45 +0400

    Update ajax2.js

--- ajax2.js
+++ ajax2.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 	$("#city").change(
 		function () {
 			sendAjaxForm('result_form', 'ajax_form2', 'action_ajax_form2.php');
+			return false;
 		}
 	);
 });
@@ -54,10 +55,58 @@ function sendAjaxForm(result_form, ajax_form, url) {
 $(document).ready(function () {
 	$("#hh").change(
 		function () {
+			if (document.getElementById("datepicker").value != '') {
+				sendAjaxForm1('result_form', 'ajax_form2', 'action_ajax_form3.php');
+			}
+			else { document.getElementById("time").disabled = true; }
+			document.getElementById("datepicker").disabled = false;
+		}
+	);
+});
+
+
+$(document).ready(function () {
+	$("#datepicker").change(
+		function () {
 			sendAjaxForm1('result_form', 'ajax_form2', 'action_ajax_form3.php');
 		}
 	);
 });
+
+$(document).ready(function () {
+	$("#buttonzap").click(
+		function () {
+			
+			if (document.getElementById('city').value != '' &&
+				document.getElementById('hh').value != '' &&
+				document.getElementById('datepicker').value != '' &&
+				document.getElementById('time').value != '') {
+				sendAjaxForm4('result_form', 'ajax_form2', 'action_ajax_form4.php');
+			
+			}
+			
+			return false;
+		}
+	);
+});
+
+function sendAjaxForm4(result_form, ajax_form, url) {
+	$.ajax({
+		url: url, //url страницы (action_ajax_form1.php)
+		type: "POST", //метод отправки
+		dataType: "html", //формат данных
+		data: $("#" + ajax_form).serialize(),  // Сеарилизуем объект
+		success: function (response) { //Данные отправлены успешно
+			result = $.parseJSON(response);
+			$('#result_form').html('Имя: |' + result.name + '|');
+
+		},
+		error: function (response) { // Данные не отправлены
+			$('#excp').html('Ошибка. Данные не отправлены.');
+		}
+	});
+}
+
 
 function sendAjaxForm1(result_form, ajax_form, url) {
 	$.ajax({
@@ -68,7 +117,7 @@ function sendAjaxForm1(result_form, ajax_form, url) {
 		success: function (response) { //Данные отправлены успешно
 			result = $.parseJSON(response);
 			$('#result_form').html('Имя: |' + result + '|');
-
+			$('#result_form').html('Имя: |' + document.getElementById('datepicker').value + '|');
 
 			//const select = document.createElement('select');
 			$(".pgp").remove();
@@ -88,12 +137,13 @@ function sendAjaxForm1(result_form, ajax_form, url) {
 				option.classList.add('pgp');
 				option.textContent = city;
 				option.value = city;
-				
+
 				select.appendChild(option);
 
 			}
 
-			document.getElementById("time").disabled = false;
+			//	document.getElementById("time").disabled = false;
+
 
 		},
 		error: function (response) { // Данные не отправлены
@@ -101,4 +151,16 @@ function sendAjaxForm1(result_form, ajax_form, url) {
 		}
 	});
 }
+
+$(document).ready(function () {
+	$("#datepicker").change(
+		function () {
+			if (document.getElementById('datepicker').value != '') {
+				document.getElementById("time").disabled = false;
+			}
+			else { document.getElementById("time").disabled = true; }
+		
+		}
+	);
+});
 
