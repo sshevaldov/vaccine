5d6a24c615d6703ec1f0d784b14dc4bd205eb5b1
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-09 22:21:13 +0400

    Update action_ajax_form4.php

--- action_ajax_form4.php
+++ action_ajax_form4.php
@@ -1,7 +1,7 @@
 <?php
 //вызывается из ajax.js из cabinet.php
 //используется для сохранения данных о заявке
-if (isset($_POST['city_selector']) && isset($_POST['place_selector']) && isset($_POST['datepicker']) && isset($_POST['time'])) {
+if (isset($_POST['city_selector']) && isset($_POST['place_selector']) && isset($_POST['datepicker']) && isset($_POST['time_selector'])) {
 
   $servername = "localhost";
   $uname = "root";
@@ -13,12 +13,12 @@ if (isset($_POST['city_selector']) && isset($_POST['place_selector']) && isset($
 
   $sql = "INSERT INTO `list` (`city_name`, `place_name`, `date`, `time`) VALUES ('{$_POST['city_selector']}', '{$_POST['place_selector']}', '{$_POST['datepicker']}', '{$_POST['time_selector']}');";
   $result = mysqli_query($link, $sql);
-  $ok = 'ok';
+  $ok1 = 'ok';
   // Формируем массив для JSON ответа
-  $result = array(
-    'name' => $ok
+  $result1 = array(
+    'name' => $ok1
   );
   //SELECT A.`time` FROM `times_pattern` A LEFT JOIN( SELECT `time` FROM `list` WHERE `city_name` = 'Ульяновск' AND `place_name` = 'Ленина' AND `date` = '04.01.2021' ) B ON A.`time` = B.`time` WHERE B.`time` IS NULL
   // Переводим массив в JSON
-  echo json_encode($result);
+  echo json_encode($result1);
 }
