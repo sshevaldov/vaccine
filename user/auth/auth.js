8fd1821025a629911064e7b29c7c4c6d47b7be5c
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-22 19:42:42 +0400

    Update auth.js

--- user/auth/auth.js
+++ user/auth/auth.js
@@ -6,26 +6,24 @@ $("#buttonToCabinet").click(//событие клинка
     }
 );
 
-
-
 function AjaxCheckInputAccount(ajax_form, url) {
     $.ajax({//ajax запрос к файлу php
         url: url,//php файл
-		type: "POST",//метод запроса
-		dataType: "html",//тип данных
-        data: $("#" + ajax_form).serialize(),
-        success: function (response) {
-            result = $.parseJSON(response);
-            log = document.getElementById('passport').value;
-            pass = document.getElementById('password').value;
-            if (log != '' && pass != '') {
-                if (result.name == 0) {
+        type: "POST",//метод запроса
+        dataType: "html",//тип данных
+        data: $("#" + ajax_form).serialize(),//собираем данные со страницы
+        success: function (response) {//если php-скрипт отработал успешно, вызываетс функция
+            log = document.getElementById('passport').value;//получаем введенный логин
+            pass = document.getElementById('password').value;//получаем введенный пароль
+            if (log != '' && pass != '') {//если логин и пароль введены
+                result = $.parseJSON(response);//получаем данные от php-скрипта
+                if (result.NumRows == 0) {//если записи с логином не найдены
                     $("#LoginErrorMessage").html('Пользователь не существует');
                 }
-                else if (result.psw == 0) {
+                else if (result.IsMatch == 0) {//если записи с логином найдены, но пароль не подходит
                     $("#LoginErrorMessage").html('Неверный пароль');
                 } else {
-                    window.location = "../cabinet/cabinet1.php";
+                    window.location = "../cabinet/cabinet1.php";//переход в кабинет администратора
                 }
             }
         }
