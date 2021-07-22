88592fbb80ff085039e38b96bccd5fe7cac75d58
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-22 19:42:45 +0400

    Update action_ajax_form.php

--- user/auth/action_ajax_form.php
+++ user/auth/action_ajax_form.php
@@ -1,31 +1,22 @@
 <?php
-session_start();
-
-if (isset($_POST["passport"]) && isset($_POST["password"])) {
-    $servername = "localhost";
-    $uname = "root";
-    $pword = "";
-    $dbname = "vaccine";
-    $psw = 0;
-    $link = mysqli_connect($servername, $uname, $pword, $dbname);
-    $sql = "SELECT * FROM `accounts` where `passport`='{$_POST["passport"]}'";
-    $result = mysqli_query($link, $sql);
-    $rows = mysqli_num_rows($result);
-    if ($rows != 0) 
-    {
-        while ($row = mysqli_fetch_array($result)) {
-            if ((password_verify($_POST["password"], $row['password']))) {
-                $psw = 1;
+require_once("../../common/funct.php"); //подключаем скрипт с функцией
+if (isset($_POST["passport"]) && isset($_POST["password"])) {//если паспорт и логин введены
+    $link = dbconnect(); //подключение к бд
+    $sql = "SELECT * FROM `accounts` where `passport`='{$_POST["passport"]}'";//запрос к записям с заданным паспортом
+    $responce = mysqli_query($link, $sql);//воспроизведение запроса
+    $NumRows = mysqli_num_rows($responce);//количество записей с заданным паспортом
+    $IsMatch = false; //флаг совпадения пароля
+    if ($NumRows != 0) {//если есть строки с заданным логином
+        while ($row = mysqli_fetch_array($responce)) {//проход по полученным записям
+            if ((password_verify($_POST["password"], $row['password']))) {//если введеный пароль совпадает
+                $IsMatch = true;//установка флага "пароль совпал"
                 $_SESSION['passport'] = $_POST["passport"];
             }
         }
     }
-   
-    $result = array(
-        'name' => $rows,
-        'psw' => $psw
+    $return = array(
+        'NumRows' => $NumRows,
+        'IsMatch' => $IsMatch
     );
-
-  
-    echo json_encode($result);
+    echo json_encode($return);
 }
