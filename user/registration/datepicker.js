a67eb9e1acf071b1d25970d7e2b6f50e85335142
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-22 22:58:54 +0400

    Create datepicker.js

---
+++ user/registration/datepicker.js
@@ -0,0 +1,30 @@
+$.datepicker.regional['ru'] = {
+    dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+    dateFormat: 'yy.mm.dd',
+    monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+    firstDay: 1,
+    changeMonth: true,
+    changeYear: true,
+    maxDate: 0,
+    yearRange: '1900:2021'
+};
+$.datepicker.setDefaults($.datepicker.regional['ru']);
+$(function () {
+    $.mask.definitions['~'] = '[]';
+    $("#date")
+        .datepicker({
+            nextText: "",
+            prevText: "",
+            dateFormat: 'yy.mm.dd',
+            changeMonth: true,
+            changeYear: true,
+            maxDate: 0,
+            maxYear: 0,
+            showAnim: "fold",
+            yearRange: '1900:2030',
+            firstDay: 1
+        })
+        .mask("~~.~~.~~", {
+            placeholder: "гг.мм.дд"
+        });
+});
