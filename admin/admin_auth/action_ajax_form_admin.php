8202596356b95d98ef4a1c264bc56f1711d42f94
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-22 19:42:53 +0400

    Update action_ajax_form_admin.php

--- admin/admin_auth/action_ajax_form_admin.php
+++ admin/admin_auth/action_ajax_form_admin.php
@@ -1,12 +1,11 @@
 <?php
 require_once("../../common/funct.php"); //подключаем скрипт с функцией
-if (isset($_POST["login"]) && isset($_POST["password"])) { //если введены логин и пароль
-    $IsMatch = false; //флаг совпадения пароля
-    $NumRows = 0; //количество строк с заданным логином
+if (isset($_POST["login"]) && isset($_POST["password"])) { //если введены логин и пароль   
     $link = dbconnect(); //подключение к бд
     $sql = "SELECT * FROM `admins` where `login`='{$_POST["login"]}'"; //запрос к записям с заданным логином
     $responce = mysqli_query($link, $sql); //воспроизведение запроса
     $NumRows = mysqli_num_rows($responce); //количество записей с заданным логином
+    $IsMatch = false; //флаг совпадения пароля
     if ($NumRows != 0) { //если есть строки с заданным логином
         while ($row = mysqli_fetch_array($responce)) { //проход по полученным записям
             if ((password_verify($_POST["password"], $row['password']))) { //если введеный пароль совпадает
