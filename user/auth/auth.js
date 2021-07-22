e84b27ac705d6a90db8323e8ab340f07abb35d8b
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-22 19:26:01 +0400

    Update auth.js

--- user/auth/auth.js
+++ user/auth/auth.js
@@ -1,35 +1,23 @@
 
-$("#buttonToCabinet").click(
+$("#buttonToCabinet").click(//событие клинка
     function () {
-        AjaxCheckInputAccount('АuthorizationForm', 'action_ajax_form.php');
-
-        return false;
-
+        AjaxCheckInputAccount('АuthorizationForm', 'action_ajax_form.php');//функция проверки наличия введенных данных
+        return false;//false для избавления от обновления страницы
     }
 );
-$('#buttonToCabinet').on('click', function () {
-    $('.table .rfield').each(function () {
-        if ($(this).val() != '') {
-            $(this).removeClass('empty_field');
-        } else {
-            $(this).addClass('empty_field');
-            $("#LoginErrorMessage").html('');
-        }
-    });
-});
+
 
 
 function AjaxCheckInputAccount(ajax_form, url) {
-    $.ajax({
-        url: url, 
-        type: "POST", 
-        dataType: "html", 
-        data: $("#" + ajax_form).serialize(),  
-        success: function (response) { 
+    $.ajax({//ajax запрос к файлу php
+        url: url,//php файл
+		type: "POST",//метод запроса
+		dataType: "html",//тип данных
+        data: $("#" + ajax_form).serialize(),
+        success: function (response) {
             result = $.parseJSON(response);
             log = document.getElementById('passport').value;
             pass = document.getElementById('password').value;
-
             if (log != '' && pass != '') {
                 if (result.name == 0) {
                     $("#LoginErrorMessage").html('Пользователь не существует');
