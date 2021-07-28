36c97beb12e7649bc2bfd8d5f8393e8fa3cc878b
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-28 23:33:21 +0400

    Update mask.js

--- common/mask.js
+++ common/mask.js
@@ -1,19 +1,14 @@
 $('.mask-pasport-division').mask('999-999');
-	$('.mask-phone').mask('+7 (999) 999-99-99');
-	$('.mask-pasport-number').mask('9999 999999');
-	$('.number').mask("9999 9999 9999 9999", {
-		placeholder: "**** **** **** ****"
-	})
+$('.mask-phone').mask('+7 (999) 999-99-99');
+$('.mask-pasport-number').mask('9999 999999');
+$('.number').mask("9999 9999 9999 9999", {
+    placeholder: "**** **** **** ****"
+})
 
-
-
-    
-
-    $(function () {
-        $.mask.definitions['~'] = '[]';
-        $("#datepicker")
-            .mask("~~.~~.~~", {
-                placeholder: "гг.мм.дд"
-            });
-    });
-    
+$(function () {
+    $.mask.definitions['~'] = '[]';
+    $("#datepicker")
+        .mask("~~.~~.~~", {
+            placeholder: "гг.мм.дд"
+        });
+});
