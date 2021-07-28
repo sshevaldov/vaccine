91bd6014ce32dc796d79282409821b3c6e3282c1
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-28 23:33:48 +0400

    Update action_ajax_form1.php

--- user/registration/action_ajax_form1.php
+++ user/registration/action_ajax_form1.php
@@ -1,11 +1,7 @@
 <?php
 require_once('../../common/funct.php');
-if (isset($_POST['ser']) && isset($_POST['sex']) && isset($_POST['password']) && isset($_POST['fam']) && isset($_POST['name']) && isset($_POST['date']) && isset($_POST['code']) && isset($_POST['omc']) && isset($_POST['phone'])) {
-  $servername = "localhost";
-  $uname = "root";
-  $pword = "";
-  $dbname = "vaccine";
-  $link = mysqli_connect($servername, $uname, $pword, $dbname);
+if (isset($_POST['ser']) && isset($_POST['sex']) && isset($_POST['password']) && isset($_POST['fam']) && isset($_POST['name']) && isset($_POST['date']) && isset($_POST['omc']) && isset($_POST['phone'])) {
+  $link = dbconnect();
   $sql = "SELECT * FROM `accounts` where `passport`='{$_POST["ser"]}'";
   $result = mysqli_query($link, $sql);
   $rows = mysqli_num_rows($result);
@@ -19,16 +15,14 @@ if (isset($_POST['ser']) && isset($_POST['sex']) && isset($_POST['password']) &&
       }
     }
     $_SESSION['fam'] = (trim($_POST['fam']));
-
-
-    $sex=($_POST['sex']);
+    $sex = ($_POST['sex']);
     $phone = ($_POST['phone']);
     $login = ($_POST['ser']);
     $oms = ($_POST['omc']);
     $password = $_POST['password'];
     $password = password_hash($password, PASSWORD_DEFAULT);
     mysqli_set_charset($link, "utf8");
-    $sql = "INSERT INTO `users`(`name`, `secondname`, `surname`, `birthdate`, `district_code`, `phone`, `passport`, `oms`,`sex`) VALUES ('$name','$otch','{$_POST['fam']}','{$_POST['date']}','{$_POST['code']}','$phone','$login','$oms','$sex');";
+    $sql = "INSERT INTO `users`(`name`, `secondname`, `surname`, `birthdate`, `phone`, `passport`, `oms`,`sex`) VALUES ('$name','$otch','{$_POST['fam']}','{$_POST['date']}','$phone','$login','$oms','$sex');";
     $result = mysqli_query($link, $sql);
     $sql = "INSERT INTO `accounts` (`passport`, `password`) VALUES ('$login', '$password');";
     $result = mysqli_query($link, $sql);
