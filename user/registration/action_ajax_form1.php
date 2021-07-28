e55544723c19982b3cf3dc2b7a9eaf3e2077ab23
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-07-28 19:47:33 +0400

    Update action_ajax_form1.php

--- user/registration/action_ajax_form1.php
+++ user/registration/action_ajax_form1.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../../common/funct.php');
-if (isset($_POST['ser']) && isset($_POST['password']) && isset($_POST['fam']) && isset($_POST['name']) && isset($_POST['date']) && isset($_POST['code']) && isset($_POST['omc']) && isset($_POST['phone'])) {
+if (isset($_POST['ser']) && isset($_POST['sex']) && isset($_POST['password']) && isset($_POST['fam']) && isset($_POST['name']) && isset($_POST['date']) && isset($_POST['code']) && isset($_POST['omc']) && isset($_POST['phone'])) {
   $servername = "localhost";
   $uname = "root";
   $pword = "";
@@ -20,13 +20,15 @@ if (isset($_POST['ser']) && isset($_POST['password']) && isset($_POST['fam']) &&
     }
     $_SESSION['fam'] = (trim($_POST['fam']));
 
+
+    $sex=($_POST['sex']);
     $phone = ($_POST['phone']);
     $login = ($_POST['ser']);
     $oms = ($_POST['omc']);
     $password = $_POST['password'];
     $password = password_hash($password, PASSWORD_DEFAULT);
     mysqli_set_charset($link, "utf8");
-    $sql = "INSERT INTO `users`(`name`, `secondname`, `surname`, `birthdate`, `district_code`, `phone`, `passport`, `oms`) VALUES ('$name','$otch','{$_POST['fam']}','{$_POST['date']}','{$_POST['code']}','$phone','$login','$oms');";
+    $sql = "INSERT INTO `users`(`name`, `secondname`, `surname`, `birthdate`, `district_code`, `phone`, `passport`, `oms`,`sex`) VALUES ('$name','$otch','{$_POST['fam']}','{$_POST['date']}','{$_POST['code']}','$phone','$login','$oms','$sex');";
     $result = mysqli_query($link, $sql);
     $sql = "INSERT INTO `accounts` (`passport`, `password`) VALUES ('$login', '$password');";
     $result = mysqli_query($link, $sql);
