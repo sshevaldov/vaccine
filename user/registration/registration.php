c189db7f4c93dc485db2df2ffac1c9cf004b8ab1
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-06 00:37:52 +0400

    Update registration.php

--- user/registration/registration.php
+++ user/registration/registration.php
@@ -31,8 +31,8 @@
 				<p><input id="name" name="name" type="text" class="rfield" onkeyup="limitInput( 'ru', this );" placeholder="Имя" style="text-transform: capitalize;" required />
 				<p><input id="otch" name="otch" type="text" onkeyup="limitInput( 'ru', this );" placeholder="Отчество (при наличии)" style="text-transform:capitalize;" />
 				<p><input id="date" name="date" autocomplete="off" type="text" class="rfield" tabindex="1" placeholder="Дата рождения" required />
-				<p><select type="text" class="rfield " id="sex" name="sex" required >
-						<option disabled selected hidden value='' >Ваш пол</option>
+				<p><select type="text" class="rfield " id="sex" name="sex" required>
+						<option disabled selected hidden value=''>Ваш пол</option>
 						<option value='Мужской'>Мужской</option>
 						<option value='Женский'>Женский</option>
 					</select></p>
@@ -45,12 +45,12 @@
 					<input id="omc" name="omc" type="text" class="number form-control rfield" placeholder="Номер полиса ОМС" required>
 			</div>
 			<div class="table">
-		
+
 				<p>Данные аккаунта</p>
 				<input id="phone" name="phone" type="text" class="rfield mask-phone form-control" placeholder="Номер телефона" required>
 				<div class="password">
 					<input id="password" type="password" class="rfield" name="password" value="" placeholder="Пароль" required>
-					<p> <input type="checkbox" onclick=" show_hide_password(this)" >Показать пароль</p>
+					<p> <input type="checkbox" id="passhide" onclick=" show_hide_password(this)">Показать пароль</p>
 				</div>
 
 
