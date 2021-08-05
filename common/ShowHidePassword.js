215c443785a8d4701a9b6d2c96f22a47e3ba4822
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-06 00:37:31 +0400

    Update ShowHidePassword.js

--- common/ShowHidePassword.js
+++ common/ShowHidePassword.js
@@ -2,11 +2,13 @@
 function show_hide_password(target) {
 	var input = document.getElementById('password');
 	if (input.getAttribute('type') == 'password') {
+	
 		target.classList.add('view');
 		input.setAttribute('type', 'text');
 	} else {
 		target.classList.remove('view');
 		input.setAttribute('type', 'password');
+		$("#passhide").attr("checked", isCheck);
 	}
 	return false;
 }
