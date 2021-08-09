9015614193bfd8166b1da8b578cdeecc678bad62
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-10 02:02:19 +0400

    Update admin_auth.php

--- admin/admin_auth/admin_auth.php
+++ admin/admin_auth/admin_auth.php
@@ -5,7 +5,6 @@
   <meta charset="utf-8">
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
   <script src="../../common/ShowHidePassword.js"></script>
-  
   <script src="../../src/jquery.maskedinput.js" type="text/javascript"></script>
   <link rel="stylesheet" type="text/css" href="../../common/style.css">
   <title>Административный интерфейс</title>
@@ -13,30 +12,29 @@
 
 <body>
   <form method="post" id="АuthorizationForm1" action="">
-    <div class="tableyktyk" style="margin-left: 15px; margin-top:15px">
-      <div class="reating-arkows zatujgdsanuk">
+    <div class="CheckBoxNight">
+      <div class="CheckBoxPageModeSub CheckBoxSub">
         <input id="CheckboxPageMode" type="checkbox" onclick="ChangePageMode()">
         <label for="CheckboxPageMode">
-          <div class="trianglesusing" data-checked="ㅤ" data-unchecked="ㅤ"></div>
+          <div class="CheckBoxRing" data-checked="ㅤ" data-unchecked="ㅤ"></div>
           <div class="moresharpened"></div>
         </label>
       </div>
     </div>
     <div class="table">
       <h1>Войти как администратор</h1>
-      <p id="LoginErrorMessage" name="LoginErrorMessage"  style="color: red;"></p>
+      <p id="LoginErrorMessage" name="LoginErrorMessage" style="color: red;"></p>
       <input id="login" name="login" type="text" value="" class="rfield" placeholder="Логин" required>
       <div class="password">
         <input id="password" name="password" type="password" value="" class="rfield" placeholder="Пароль" required>
-      
       </div>
-      <p > <input type="checkbox" id="passhide" onclick=" show_hide_password(this)">Показать пароль</p>
+      <p> <input type="checkbox" id="passhide" onclick=" show_hide_password(this)">Показать пароль</p>
       <button type="submit" id="buttonToAdminCabinet" class="btn_submit disabled">Войти</button>
-    </div>      
+    </div>
   </form>
   <div class="table-help">
-      <a href="../../user/auth/auth.php">Пользовательский интерфейс</a>
-    </div>
+    <a href="../../user/auth/auth.php">Пользовательский интерфейс</a>
+  </div>
   <script src="../../common/PageMode.js"></script>
   <script src="../../common/RedError.js"></script>
   <script src="admin_auth.js"></script>
