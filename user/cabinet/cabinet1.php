e6992c3f3e5cb08240de98e4ac0a3a4606466fcd
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-10 02:02:51 +0400

    Update cabinet1.php

--- user/cabinet/cabinet1.php
+++ user/cabinet/cabinet1.php
@@ -3,7 +3,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <script src="../../lib/jquery-1.8.3.min.js" type="text/javascript" charset="utf-8"></script>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
     <script src="../../src/jquery.maskedinput.js" type="text/javascript"></script>
     <link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/redmond/jquery-ui.css" />
@@ -11,16 +10,15 @@
     <title>Личный кабинет</title>
     <?php require_once('../../common/funct.php') ?>
     <?php require_once('showuser.php') ?>
-
 </head>
 
 <body>
     <form method="post" id="CabinetForm" action="">
-        <div class="tableyktyk" style="margin-left: 15px; margin-top:15px">
-            <div class="reating-arkows zatujgdsanuk">
+        <div class="CheckBoxNight">
+            <div class="CheckBoxPageModeSub CheckBoxSub">
                 <input id="CheckboxPageMode" type="checkbox" onclick="ChangePageMode()">
                 <label for="CheckboxPageMode">
-                    <div class="trianglesusing" data-checked="ㅤ" data-unchecked="ㅤ"></div>
+                    <div class="CheckBoxRing" data-checked="ㅤ" data-unchecked="ㅤ"></div>
                     <div class="moresharpened"></div>
                 </label>
             </div>
