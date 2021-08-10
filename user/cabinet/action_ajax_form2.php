b61051bc21f844921e96ec9bf2aea8cac5aa9118
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-11 01:12:34 +0400

    Update action_ajax_form2.php

--- user/cabinet/action_ajax_form2.php
+++ user/cabinet/action_ajax_form2.php
@@ -1,7 +1,10 @@
 <?php
 require_once("../../common/funct.php");
 if (isset($_POST['city_selector'])) { //если город установлен
-  $link = dbconnect(); //подключение к бд
+  // $link = dbconnect(); //подключение к бд
+  include("../../f.php");
+    $link = new Dbconnect();
+    $link=$link->dbconnect();
   mysqli_set_charset($link, "utf8"); //установка кодовой страницы подключения
   $city_selector = $_POST['city_selector']; //установленное значение города
   $sql = "SELECT * FROM `places` where `city_name`= '{$city_selector}'"; //выводим адреса в заданном городе
