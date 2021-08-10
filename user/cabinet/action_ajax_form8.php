1f76f4a074df90961f998bc784e7546b4c875ddc
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-11 01:12:49 +0400

    Update action_ajax_form8.php

--- user/cabinet/action_ajax_form8.php
+++ user/cabinet/action_ajax_form8.php
@@ -1,7 +1,10 @@
 <?php
 require_once("../../common/funct.php"); //подключение файла с функцией
 if (isset($_POST['place_selector1'])) { //если адрес задан
-  $link = dbconnect(); //соединение с бд
+  // $link = dbconnect(); //соединение с бд
+  include("../../f.php");
+    $link = new Dbconnect();
+    $link=$link->dbconnect();
   mysqli_set_charset($link, "utf8"); //установка кодовой страницы подключения  
   // вывод свободных времен для записи в заданный день:
   $sql = "SELECT A.`time` FROM `times_pattern` A LEFT JOIN( SELECT `time` FROM `list` WHERE `city_name` = '{$_POST['city_selector1']}' AND `place_name` = '{$_POST['place_selector1']}' AND `date` = '{$_POST['datepickerVak1']}' ) B ON A.`time` = B.`time` WHERE B.`time` IS NULL";
