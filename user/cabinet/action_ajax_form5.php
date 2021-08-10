b39643152eb3ce9aef90a1b69b8c6b6cce0f173b
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-11 01:12:43 +0400

    Update action_ajax_form5.php

--- user/cabinet/action_ajax_form5.php
+++ user/cabinet/action_ajax_form5.php
@@ -1,6 +1,9 @@
 <?php
 require_once("../../common/funct.php"); //подключение файла с функцией
-$link = dbconnect(); //соединение с бд
+// $link = dbconnect(); //соединение с бд
+include("../../f.php");
+    $link = new Dbconnect();
+    $link=$link->dbconnect();
 mysqli_set_charset($link, "utf8"); //установка кодовой страницы подключения
 $sql = "SELECT * FROM `users` where `passport`='{$_SESSION['passport']}'"; //вывести запись пользователя по паспорту
 $result = mysqli_query($link, $sql); //воспроизведение запроса
