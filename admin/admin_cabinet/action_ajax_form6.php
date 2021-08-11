6faf317c531560819a8153b6d48d37e60592081c
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-11 19:53:41 +0400

    Update action_ajax_form6.php

--- admin/admin_cabinet/action_ajax_form6.php
+++ admin/admin_cabinet/action_ajax_form6.php
@@ -4,6 +4,7 @@ if (true) {
   $_SESSION['sort'] = $_POST['sort'];
   $_SESSION['dp1'] = $_POST['datepicker_startDate'];
   $_SESSION['dp2'] = $_POST['datepicker_endDate'];
+  
   $_SESSION['city_sel'] = $_POST['city_selector'];
   $_SESSION['pl_sel'] = $_POST['place_selector'];
   $_SESSION['t_sel'] = $_POST['time_selector'];
