08ce06cb5f87f606bbb02f852b8879afdb93efe8
Шевалдов Станислав Сергеевич <sshevaldov@yandex.ru>
2021-08-16 17:31:44 +0400

    Update PageMode.js

--- common/PageMode.js
+++ common/PageMode.js
@@ -1,5 +1,7 @@
 window.onload = function () {
+    console.log(localStorage.getItem('mode'));
     switch (localStorage.getItem('mode')) {
+      
         case "dark":
             SetPageMode("#132f56", "dark", true);
             break;
@@ -17,10 +19,28 @@ function ChangePageMode() {
 }
 
 function SetPageMode(color, mode, isCheck) {
-    var a = document.getElementsByTagName('a');
-    for (var i = 0; i < a.length; i++) {
-        a[i].style.color = document.body.style.backgroundColor;
-    }
+    console.log(localStorage.getItem('mode'))
+    switch(mode) {
+        case "light": {
+            var a = document.getElementsByTagName('a');
+            for (var i = 0; i < a.length; i++) {
+                a[i].style.color = "#132f56";
+            }
+          break;
+        }
+        case "dark": {
+            var a = document.getElementsByTagName('a');
+            for (var i = 0; i < a.length; i++) {
+                a[i].style.color = "lightblue";
+            }
+          break;
+        }
+        
+      }
+    // var a = document.getElementsByTagName('a');
+    // for (var i = 0; i < a.length; i++) {
+    //     a[i].style.color = document.body.style.backgroundColor;
+    // }
     document.body.style.backgroundColor = color;
     localStorage.setItem('mode', mode);
     $("#CheckboxPageMode").attr("checked", isCheck);
